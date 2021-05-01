52a5946b8b1d2a08b49032285dd6d528e9792811
winters0727 <winters0727@gmail.com>
2021-05-01 20:59:37 +0900

    Update LinkedList

--- practice/linkedList.py
+++ practice/linkedList.py
@@ -1,26 +1,91 @@
-class LinkedList:
-    def __init__(self, head=None):
-        self.head = head
-
 class Node:
     def __init__(self, value=None):
         self.value = value
         self.next = None
 
-linked_list = LinkedList()
+class LinkedList:
+    def __init__(self, array=None):
+        self.head = None
+        if array:
+            for value in array:
+                if not self.head:
+                    self.head = Node(value)
+                    head = self.head
+                else:
+                    head.next = Node(value)
+                    head = head.next
 
-for num in range(1,10001):
-    node = Node(num)
-    if not linked_list.head:
-        linked_list.head = node
-        now = linked_list.head
-    else:
-        now.next = node
-        now = node
+    def __len__(self):
+        if not self.head:
+            return 0
+        length = 1
+        head = self.head
+        while head.next:
+            head = head.next
+            length += 1
+        return length
 
-head = linked_list.head
-while head.next:
-    print(head.value)
-    head = head.next
-else:
-    print(head.value)
+    def __str__(self):
+        if self.empty():
+            return '[]'
+        result = '['
+        head = self.head
+        while head:
+            result += f'{str(head.value)} '
+            head = head.next
+        result = result[:-1] + ']'
+        return result
+
+    def empty(self):
+        if self.head:
+            return False
+        return True
+    
+    def insert(self, value):
+        if self.empty():
+            self.head = Node(value)
+            return self.head
+
+        head = self.head
+        
+        while head.next:
+            head = head.next
+        head.next = Node(value)
+        
+        return head.next.value
+
+    def pop(self):
+        if self.empty():
+            raise Exception('LinkedList is empty!')
+        head = self.head
+        if not head.next:
+            return head
+
+        while head.next.next:
+            head = head.next
+
+        result = head.next.value
+        head.next = None
+
+        return result
+
+    def find(self, value):
+        if self.empty():
+            raise Exception('LinkedList is empty!')
+        head = self.head
+        while head:
+            if head.value == value:
+                return True
+            head = head.next
+        return False
+
+    def tail(self):
+        if self.empty():
+            return None
+
+        head = self.head
+        
+        while head.next:
+            head = head.next
+        
+        return head.value
